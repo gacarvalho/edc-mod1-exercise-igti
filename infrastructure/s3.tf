8fb9b68b6f94c4638556085a90bcda4b294a7243
Gabriel Carvalho <73447354+gacarvalho@users.noreply.github.com>
2022-03-13 19:32:31 -0300

    Update s3 - correcao

--- infrastructure/s3.tf
+++ infrastructure/s3.tf
@@ -1,19 +1,12 @@
 resource "aws_s3_bucket" "dl" {
   bucket = "datalake-sesson-edc-tf"
-  # acl    = "private"
+  acl    = "private"
 
   tags = {
     IES   = "IGTI",
     CURSO = "EDC"
   }
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
+  
 }
 
 
