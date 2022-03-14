2eb41ab2ced1fba78911e1a7b2d7e20bfc55a5c0
Gabriel Carvalho <73447354+gacarvalho@users.noreply.github.com>
2022-03-13 22:33:22 -0300

    Upload s3 files - correcao

--- infrastructure/s3_files.tf
+++ infrastructure/s3_files.tf
@@ -14,14 +14,6 @@ resource "aws_s3_bucket_object" "delta_insert" {
     etag = filemd5("../etl/01_delta_spark_insert.py")
 }
 
-resource "aws_s3_bucket_object" "delta_insert" {
-    bucket = aws_s3_bucket.dl.id
-    key = "emr-code/pyspark/01_delta_spark_insert.py"
-    acl = "private"
-    source = "../etl/01_delta_spark_insert.py"
-    etag = filemd5("../etl/01_delta_spark_insert.py")
-}
-
 resource "aws_s3_bucket_object" "delta_upsert" {
     bucket = aws_s3_bucket.dl.id
     key = "emr-code/pyspark/02_delta_spark_upsert.py"
