bcceb5c553f90b6c46503232e6a0bf483936453d
Gabriel Carvalho <gacarvalho.contato@gmail.com>
2022-03-13 22:28:21 -0300

    Upload - use case

--- infrastructure/s3.tf
+++ infrastructure/s3.tf
@@ -2,7 +2,14 @@ resource "aws_s3_bucket" "dl" {
   bucket = "datalake-sesson-edc-tf"
 
   tags = {
+<<<<<<< HEAD
     IES   = "SESSON",
     CURSO = "EDC"
   }
 }
+=======
+    IES   = "IGTI",
+    CURSO = "EDC"
+  }
+}
+>>>>>>> b9c67797180a7b812e13766cfa0c8c497799f4ff
